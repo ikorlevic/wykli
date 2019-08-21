8b414ce5b321809bbd6b2fab6c5c3590e428fbab
Iva <iva.korlevic@gmail.com>
2019-08-21 22:32:05 +0200

    adjustments to run in browser

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-require ('./app/index.js');
+const http = require('http');
+
+const hostname = 'proodos.hr';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
