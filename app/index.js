7400bba3a3a08fe700b8602019bb61fbf696b28d
Iva <iva.korlevic@gmail.com>
2019-08-21 23:17:10 +0200

    main view

--- app/index.js
+++ app/index.js
@@ -1,18 +1,11 @@
+require('handlebars');
+require('mongodb');
 var mvdom = require("mvdom");
 
-class HelloView {
-    constructor(userName){
-        this.userName = userName;
-    }
-    create(opts){
-        return `<div class='HelloView'>
-      <h1>Hello ${this.userName}. Here your todos:<h2>
-      <todo-list></todo-list>
-    </div>`;
+class MainView{
+    create(){
+        return "hello"
     }
 }
 
-mvdom.display(new HelloWorld('John'), 'body'); // by default will append last ('last') to body
-
-// or to empty the parent (here 'body') container
-mvdom.display(new HelloWorld('John'), 'body', 'empty');
+mvdom.display(new MainWiew(), 'main'); // by default will append last ('last') to body
