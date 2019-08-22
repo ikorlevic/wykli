09f08fde839e4bfb48085dad76a79af9742424b1
Iva <iva.korlevic@gmail.com>
2019-08-22 18:28:44 +0200

    adjustemnts to module

--- app/index.js
+++ app/index.js
@@ -1,18 +1,18 @@
 require('handlebars');
 require('mongodb');
-import { display, remove, View } from "mvdom";
+var mvdom = require("mvdom");
 
 class MainView implements View{
     constructor(data){
         this.data = data
-    },
+    }
     init(){
         var view = this; // best practice
         view.el;
-    },
+    }
     create(){
         return '<div id="main">${this.data}</div>'
     }
 }
 
-display(new MainView('data'),'body','first'); // by default will append last ('last') to body
+mvdom.display(new MainView('data'),'body','first'); // by default will append last ('last') to body
